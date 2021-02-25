1a346652defaf75dfaaf06ff9cccbb5d464a7750
KAnanev <russrage@gmail.com>
2021-02-25 11:56:48 +0300

    test.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,38 +1,7 @@
 from scripts.class_user import User
-from scripts.class_user import UserMain
-from scripts.functions_find_people import sex_pair_num
 
 
 import unittest
-
-api = ''
-name = ''
-
-
-class TestUser(unittest.TestCase):
-    def setUp(self) -> None:
-        self.user = User(api)
-
-    def test_version(self):
-        self.assertTrue(type(self.user.version) == str,
-                        'Версия приложения должна быть строкой!')
-
-
-class TestUser_Main(unittest.TestCase):
-    def setUp(self) -> None:
-        self.test_user = UserMain(api)
-
-    def test_user_id(self):
-        self.test_user.get_user(name)
-        self.assertTrue(type(self.test_user.user_id) == int,
-                        'Не работает get_user')
-
-
-class TestFunc(unittest.TestCase):
-    def test_sex_pair_num(self):
-        self.assertTrue(sex_pair_num(1) == 2, 'Ошибка')
-        self.assertTrue(sex_pair_num(2) == 1, 'Ошибка')
-
 
 if __name__ == '__main__':
     unittest.main()
